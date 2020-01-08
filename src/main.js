ffeda372aaf059a600f8e207fa5ffd748854e207
L2JiHan <619934220@qq.com>
2020-01-08 16:35:48 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import FastClick from "fastclick";
+import "./assets/styles/reset.css";
+import "./assets/styles/border.css";
+
+FastClick.attach(document.body);
+
 Vue.config.productionTip = false;
 
 new Vue({
